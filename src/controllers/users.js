f41bc71ff692ead1fddb8776c14605ad0bb3fe6f
BlueSkunka <quentin.somveille3874@gmail.com>
2024-10-08 18:44:58 +0200

    feat: init mjml template

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,6 +1,7 @@
 import User from "../models/users.js";
 import { Op } from "sequelize";
 import nodemailer from "nodemailer";
+import mjml2html from 'mjml'
 
 async function generateID(id) {
 	const { count } = await findAndCountAllUsersById(id);
@@ -89,12 +90,110 @@ export async function registerUser(userDatas, bcrypt) {
 			port: 1025,
 		}
 	)
-	
+
+	const htmlOutput = mjml2html(`
+<mjml>
+  <mj-head>
+    <mj-title>Welcome to PokeBattle !</mj-title>
+    <mj-font name="Roboto" href="https://fonts.googleapis.com/css?family=Roboto:300,500"></mj-font>
+    <mj-attributes>
+      <mj-all font-family="Roboto, Helvetica, sans-serif"></mj-all>
+      <mj-text font-weight="300" font-size="16px" color="#616161" line-height="24px"></mj-text>
+      <mj-section padding="0px"></mj-section>
+    </mj-attributes>
+  </mj-head>
+  <mj-body>
+    <mj-section padding="20px 0">
+      <mj-column width="60%">
+        <mj-text font-size="10px">Start creating your own pokemon teams right now !</mj-text>
+      </mj-column>
+      <mj-column width="40%">
+        <mj-text align="right">locahost:5173</mj-text>
+      </mj-column>
+    </mj-section>
+    <mj-section>
+      <mj-column width="100%">
+        <mj-image href="" src="" alt="PokeBattle"></mj-image>
+      </mj-column>
+    </mj-section>
+    <mj-section>
+      <mj-column width="100%">
+        <mj-text align="center">Satisfy your Pokemon Battle passion !</mj-text>
+      </mj-column>
+    </mj-section>
+    <mj-section>
+      <mj-column width="45%">
+        <mj-text align="center" font-weight="500" padding="0px" font-size="18px">Welcome {{username}}</mj-text>
+        <mj-divider border-width="2px" border-color="#616161"></mj-divider>
+        <mj-divider border-width="2px" border-color="#616161" width="45%"></mj-divider>
+      </mj-column>
+    </mj-section>
+    <mj-section padding-top="30px">
+      <mj-column width="100%">
+        <mj-text>
+          <p>Hello there!</p>
+          <p> Welcome to the world of Pokemon ! You joinded the PokeBattle league specialise into High Ranking Pokemon Battle, even harder than the classic Pokemon league ! </p>
+          <p>But before we can let you join the PokeBattle league, we require you validate your email through the bellow link: </p>
+          <p>Once it's done, you may start to create your best Pokemon Teams and battle every dressors around the world !</p>
+        </mj-text>
+      </mj-column>
+    </mj-section>
+    <mj-section>
+      <mj-column width="11%">
+        <mj-image padding-right="0px" padding-left="25px" align="left" width="70px" href="" src="" alt="Mew"></mj-image>
+      </mj-column>
+      <mj-column width="89%">
+        <mj-text padding="0 25px">
+          <p style="color:#BDBDBD; line-height: 9px"> Red - <a href="localhost:5173" style="color: #3498DB;">
+              PokeBattle
+            </a>  Champion </p>
+          <p style="font-style: italic; color:#BDBDBD; line-height: 9px">Pikechu use lightning !</p>
+        </mj-text>
+      </mj-column>
+    </mj-section>
+    <mj-section>
+      <mj-column width="100%">
+        <mj-divider border-width="1px" border-color="#E0E0E0"></mj-divider>
+      </mj-column>
+    </mj-section>
+    <mj-section>
+      <mj-column width="75%">
+        <mj-text>
+          <h3 style="font-weight: bold; margin-top: 0; margin-bottom: 0"> <a href="localhost:5173" style="color: #3498DB; text-decoration: none">
+              First pokemon gen is available !
+            </a> </h3>
+          <p style="font-size: 14px">We allow you to choose between 151 species of Pokemon to build up your team ! Next gen will be available soon !</p>
+        </mj-text>
+      </mj-column>
+      <mj-column width="25%">
+        <mj-image width="100px" href="" alt="Mew"></mj-image>
+      </mj-column>
+    </mj-section>
+    <mj-section>
+      <mj-column width="75%">
+        <mj-text>
+          <h3 style="font-weight: bold; margin-top: 0; margin-bottom: 0"> <a href="localhost:5173" style="color: #3498DB; text-decoration: none;">
+              Battle against your firends !
+            </a> </h3>
+          <p style="font-size: 14px;">With a matchmaking lobby, you may battle with your best friend or with anyon around the world !</p>
+        </mj-text>
+      </mj-column>
+      <mj-column width="25%">
+        <mj-image width="100px" href="" src="" alt="Mew"></mj-image>
+      </mj-column>
+    </mj-section>
+  </mj-body>
+</mjml>
+	`, {
+		keepComments: false,
+		minify: true
+	});
+
 	transporter.sendMail({
 		from: 'pokebattle@yopmail.com',
 		to: createdUser.email,
 		subject: 'Validation du compte PokeBattle',
-		html: 'mjml'
+		html: htmlOutput.html
 	})
 
 	return createdUser;
