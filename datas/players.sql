3da25820053cced7479d4a9561d2ff651b6d637e
BlueSkunka <quentin.somveille3874@gmail.com>
2025-02-22 19:35:22 +0100

    fix: players.sql

---
+++ datas/players.sql
@@ -0,0 +1,5 @@
+insert into users (id, username, firstname, lastname, email, password, bestScrore, verified, createdAt, updatedAt)
+values
+('AZUOND', 'Ondine', 'Ondine', 'Azuria', 'ondine@yopmail.com', '$2a$12$sir6G3k3aWiFBSIopBIZWeO1/6KiZE5n/k293/FiJavb9WMrXsrB2', null, 1, now(), now()),
+('PALSAC', 'Sacha', 'Sacha', 'Palette', 'sacha@yopmail.com', '$2a$12$sir6G3k3aWiFBSIopBIZWeO1/6KiZE5n/k293/FiJavb9WMrXsrB2', null, 1, now(), now())
+;
